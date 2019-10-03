a474cc592e26553364ddc274d704c3ce4437a2d4
Gotzsche <pierregotzsche@gmail.com>
2019-10-03 15:29:53 +0200

    second commit BD ok

--- routes/subjects.js
+++ routes/subjects.js
@@ -1,27 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var ObjectID = require('mongodb').ObjectID;
 
-
-/* GET /subjects */
+/* GET subjects. */
 router.get('/', function (req, res, next) {
     const { db } = req.app.locals;
-    db.collection('subjects').insertOne(req.body, (err, subject) => res.json(subject));
+    db.collection('subjects').find().toArray((err, subjects) => res.json(subjects));
 });
 
 /* GET /subjects/:id */
-router.get('/:id', function (req, res, next) {
-    const { db } = req.app.locals;
-    const { id } = req.params;
-    db.collection('subjects').updateOne({ _id: new ObjectID(id) }, { $set: req.body }, (err, subject) => res.json(subject));
-});
 
 /* PUT /subjects/:id */
-router.get('/:id', function (req, res, next) {
-    const { db } = req.app.locals;
-    const { id } = req.params;
-    db.collection('subjects').updateOne({ _id: new ObjectID(id) }, (err, response) => res.json(response));
-});
+
 
 module.exports = router;
-
